72c3790417a465a5c53c45e19fc3225b90b9bf6f
Olex <tholex@gmail.com>
2011-03-21 00:43:26 -0400

    Major Add-ons, Sun & Moon, BG transitions

--- js/script.js
+++ js/script.js
@@ -11,8 +11,33 @@
   TARGET_FPS = 30,
   horizonY = window.innerHeight * (.7),
   context,
-  offsetTime = 0.0,
   memoTime = 0.0;
+  window.offsetTime = 0.0;
+  var COLOR_FOR_HOUR = [ [120, 28, 58],
+                         [69,21,61],
+                         [37,0,70],
+                         [24,0,64],
+                         [110,48,149],
+                         [174,129,248],
+                         [25,229,169],
+                         [149,209,44],
+                         [48,209,23],
+                         [0,255,0],
+                         [0,255,255],
+                         [0,182,255],
+                         [0,129,255],
+                         [75,81,242],
+                         [229,119,255],
+                         [255,92,92],
+                         [255,110,38],
+                         [255,131,35],
+                         [255,190,0],
+                         [140,125,84],
+                         [89,133,110],
+                         [52,78,120],
+                         [37,36,120],
+                         [153,61,147]];
+  
 
 window.landscape = function() {
   /* setup the landscape canvas, etc */
@@ -40,6 +65,10 @@ function setupAnimation(){
   window.drawLandscape();
 }
 
+window.drawClock = function(){
+  // output internal 'timeOfDay' in HR:MN format
+  $('#clock').html( parseInt(timeOfDay()*24)+ ":" + parseInt(timeOfDay()*1440)%60 );
+}
 
 window.drawLandscape = function() {
   window.drawTimer = null;
@@ -58,20 +87,63 @@ function clearCanvas(){
 
 function drawSky(){
   drawSkyGradient();
+  drawMoonGlow();
+  drawMoon();
+  drawSunGlow();
   drawSun();
 }
 
 function drawSkyGradient(){
   var backgroundGradient = context.createLinearGradient(0, 0, 0, horizonY);
-  backgroundGradient.addColorStop(0, "rgb(255, 190, 0)");
-  backgroundGradient.addColorStop(1, "rgb(212, 110, 38)");
+  // top of the sky = sky color at the moment
+  backgroundGradient.addColorStop(0, skyColorFromTime(timeOfDay()));
+  // bottom of the sky = sky color ~20+ mins from now
+  backgroundGradient.addColorStop(1, skyColorFromTime(timeOfDay()+ .02)); 
   context.fillStyle = backgroundGradient;
   context.fillRect(0, 0, SCREEN_WIDTH, horizonY);
 }
 
+function skyColorFromTime(tod) {
+  //tod is 0-1 for 24 hours
+  var proportionOfHour = tod * 24 % 1;
+  var previousHourColor = COLOR_FOR_HOUR[parseInt(tod*24)];
+  // parse current hour, add one, with 0-23 wraparound
+  var nextHourColor = COLOR_FOR_HOUR[ (parseInt(tod*24)+1)%24 ];
+  return lerpColor(previousHourColor, nextHourColor, proportionOfHour);
+}
+
+function drawSunGlow(){
+  var sunGlowGradient = context.createRadialGradient(sunX(), sunY(), 0, sunX()+Math.random()*2, sunY()+Math.random()*2, sunGlowRadius());
+  sunGlowGradient.addColorStop(0, "rgba(255,40,0,1)");
+  sunGlowGradient.addColorStop(.4, "rgba(255, 32, 0, .8)");
+  sunGlowGradient.addColorStop(1, "rgba(255,40,0,0)");
+  context.fillStyle = sunGlowGradient;
+  context.fillRect(0, 0, SCREEN_WIDTH, horizonY);
+}
+
+function sunGlowRadius(){
+  var tod = timeOfDay();
+  if( (tod < .20) || (tod > .8)){
+    return 50;
+  }
+  return Math.abs(Math.sin(tod*Math.PI*2))*220;
+}
+
 function drawSun(){
-  context.fillStyle = "rgba(255,255,170)";
-  context.arc(200, 200, 30, 0, Math.PI*2, 1);
+  context.fillStyle = "rgba(255,255,175, 0.95)";
+  context.beginPath();
+  context.arc(sunX(), sunY(), 30, 0, Math.PI*2, 1);
+  context.fill();
+}
+
+function drawMoonGlow(){
+
+}
+
+function drawMoon(){
+  context.fillStyle = "rgba(202,202,202, 0.95)";
+  context.beginPath();
+  context.arc(moonX(), moonY(), 30, 0, Math.PI*2, 1);
   context.fill();
 }
 
@@ -83,8 +155,29 @@ function drawGround(){
   context.fillRect(0, horizonY, SCREEN_WIDTH, (SCREEN_HEIGHT-horizonY));
 }
 
-window.drawClock = function(){
-  $('#clock').html( timeOfDay() );
+/* Processing Originals */
+function sunY(){
+  return SCREEN_HEIGHT*.7 + Math.sin(timeOfDay()*Math.PI*2 + Math.PI/2)*SCREEN_HEIGHT*0.6;
+}
+function sunX(){
+  return SCREEN_WIDTH*.5 - Math.sin(timeOfDay()*Math.PI*2)*SCREEN_WIDTH*0.3;
+}
+function moonY(){
+  return SCREEN_HEIGHT*.7 + Math.sin(timeOfDay()*Math.PI*2 - Math.PI/2)*SCREEN_HEIGHT*.6;
+}
+function moonX(){
+  return SCREEN_WIDTH*.5 + Math.sin(timeOfDay()*Math.PI*2)*SCREEN_WIDTH*0.3;
+}
+
+window.lerpColor = function(rgb1, rgb2, proportion){
+  // Linear Interpolation between two rgb colors,
+  // args: [#,#,#], [#,#,#], 0-1.0 float
+  // returns CSS-parseable string like "rgb(24, 56, 220)"
+  var newRGB = [0, 0, 0];
+  for(var i = 0; i< newRGB.length; i++){
+    newRGB[i] = parseInt(rgb1[i]*(1.0-proportion) + rgb2[i]*proportion);
+  }
+  return "rgb(".concat(newRGB.join(",") , ")");
 }
 
 function onWindowResize(){
@@ -108,8 +201,11 @@ function onWindowResize(){
 
 function timeOfDay(){
   var d_now = new Date();
-  var seconds_past = d_now.getHours()*3600.0+d_now.getMinutes()*60.0 + d_now.getSeconds() + d_now.getMilliseconds()/1000.0;
-  return (seconds_past)/(24*3600.0);
+  var seconds_past = d_now.getHours()*3600.0 +
+                     d_now.getMinutes()*60.0 +
+                     d_now.getSeconds() +
+                     d_now.getMilliseconds()/1000.0;
+  return ((seconds_past)/(24*3600.0)+ window.offsetTime)%1;
 }
 
 
